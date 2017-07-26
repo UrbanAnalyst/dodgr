ff784b813f1166d36c220d9002e5150466650b55
mpadge <mark.padgham@email.com>
2017-07-26 12:30:55 +0200

    fix heap choice implementation

--- R/test.R
+++ R/test.R
@@ -3,13 +3,13 @@
 #' @param graph \code{data.frame} object representing the network graph
 #' @param heap Type of heap to use in priority queue. Options include
 #' Fibonacci Heap (default; \code{FHeap}), Binary Heap (\code{BHeap}),
-#' \code{Radix}, Trinomial Heap (\code{Tri}), Extended Trinomial Heap
-#' (\code{TriExt}, and 2-3 Heap (\code{Heap23}).
+#' \code{Radix}, Trinomial Heap (\code{TriHeap}), Extended Trinomial Heap
+#' (\code{TriHeapExt}, and 2-3 Heap (\code{Heap23}).
 #' @return square matrix of distances between nodes
 #'
 #' @export
 test <- function(graph, heap = 'FHeap') {
-    heaps <- c ("FHeap", "BHeap", "Radix", "Tri", "TriExt", "Heap23")
+    heaps <- c ("FHeap", "BHeap", "Radix", "TriHeap", "TriHeapExt", "Heap23")
     heap <- match.arg (arg = heap, choices = heaps)
 
     d <- rcpp_get_sp (graph, heap)
