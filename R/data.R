854c47b1b111ea11591f2fc3687117470f908e8d
Andreas Petutschnig <andreas@petutschnig.de>
2017-07-28 17:20:57 +0200

    add weighting profiles

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Weighting profiles
+#'
+#' Collection of weighting profiles used to adjust the routing process to
+#' different means of transport. Original data taken from the Routino project.
+#'
+#' @format \code{data.frame} with profile names, means of transport and
+#' weights.
+#' @references \url{https://www.routino.org/xml/routino-profiles.xml}
+"weighting_profiles"
