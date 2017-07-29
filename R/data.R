1ccfcec6c42320868ccfe7977f3a8a06ead318e0
mpadge <mark.padgham@email.com>
2017-07-29 22:45:49 +0200

    put wt-profiles doc in R/dodgr-package.R

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' Weighting profiles
-#'
-#' Collection of weighting profiles used to adjust the routing process to
-#' different means of transport. Original data taken from the Routino project.
-#'
-#' @format \code{data.frame} with profile names, means of transport and
-#' weights.
-#' @references \url{https://www.routino.org/xml/routino-profiles.xml}
-"weighting_profiles"
--- R/dodgr-package.r
+++ R/dodgr-package.r
@@ -9,3 +9,16 @@
 #' @importFrom Rcpp evalCpp
 #' @useDynLib dodgr, .registration = TRUE
 NULL
+
+#' weighting_profiles
+#'
+#' Collection of weighting profiles used to adjust the routing process to
+#' different means of transport. Original data taken from the Routino project.
+#'
+#' @name weighting_profiles
+#' @docType data
+#' @keywords datasets
+#' @format \code{data.frame} with profile names, means of transport and
+#' weights.
+#' @references \url{https://www.routino.org/xml/routino-profiles.xml}
+NULL
