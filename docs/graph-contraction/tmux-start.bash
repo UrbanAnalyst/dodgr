d161580e1a826682682ff1c3631f8701b3fcb98b
mpadge <mark.padgham@email.com>
2017-08-09 14:59:39 +0200

    update docs/graph-contraction
    totally revised approach with just one map and the internal edge lists of replacement edges

--- docs/graph-contraction/tmux-start.bash
+++ docs/graph-contraction/tmux-start.bash
@@ -10,6 +10,7 @@ tmux send-keys -t $SESSION:1 ':' 'tabe body.tex' C-m
 tmux send-keys -t $SESSION:1 ':' 'tabe fig1.tex' C-m
 tmux send-keys -t $SESSION:1 ':' 'tabe fig2.tex' C-m
 tmux send-keys -t $SESSION:1 ':' 'tabe fig3.tex' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe fig4.tex' C-m
 tmux send-keys -t $SESSION:1 'gt' C-m
 tmux send-keys -t $SESSION:1 'gt' C-m
 tmux split-window -h -p 20
