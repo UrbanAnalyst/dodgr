9c1837d1f457faf7330ed0c71b9a5067789233d5
mpadge <mark.padgham@email.com>
2017-08-23 19:16:19 +0200

    fix tests/valgrind-script

--- tests/valgrind-script.R
+++ tests/valgrind-script.R
@@ -1,9 +1,10 @@
-devtools::load_all ("../dodgr")
+#devtools::load_all ("../dodgr")
+library (dodgr)
 #streetnet <- dodgr_streetnet ("hampi india")
 #nms <- c ("osm_id", "highway", "oneway", "geometry")
 #streetnet <- streetnet [names (streetnet) %in% nms, ]
 #save (streetnet, file = "hampi.rda")
-load ("tests/hampi.rda")
+load ("hampi.rda")
 graph <- weight_streetnet (streetnet)
 graph <- dodgr_sample (graph, nverts = 100)
 d <- dodgr_dists (graph)
