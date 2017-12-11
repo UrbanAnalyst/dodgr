f7df43de4b6d4b7a8fe3f613664458fc6fc3c850
mpadge <mark.padgham@email.com>
2017-12-11 11:20:37 +0100

    Rcpp vectors are init length 0

--- src/sf-as-network.cpp
+++ src/sf-as-network.cpp
@@ -46,9 +46,9 @@ Rcpp::List rcpp_sf_as_network (const Rcpp::List &sf_lines,
         if (nms [i] == "highway")
             highway_index = static_cast <int> (i);
     }
-    Rcpp::CharacterVector ow = NULL;
-    Rcpp::CharacterVector owb = NULL;
-    Rcpp::CharacterVector highway = NULL;
+    Rcpp::CharacterVector ow; // init length = 0
+    Rcpp::CharacterVector owb;
+    Rcpp::CharacterVector highway;
     if (one_way_index >= 0)
         ow = sf_lines [one_way_index];
     if (one_way_bicycle_index >= 0)
