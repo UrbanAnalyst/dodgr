36c4d16fd5e8f52d13cfe2bd16928687a18b2e60
mpadge <mark.padgham@email.com>
2017-12-15 11:48:05 +0100

    add bbox arg to dodgr_flowmap

--- R/flowmap.R
+++ R/flowmap.R
@@ -5,20 +5,23 @@
 #' @param net A street network with a \code{flow} column obtained from
 #' \code{dodgr_flows}
 #' @param filename Name of \code{.png} file to write to
+#' @param bbox If given, scale the map to this bbox, otherwise use entire extend
+#' of \code{net}
 #' @param linescale Maximal thickness of plotted lines
 #'
 #' @note \code{net} should be first passed through \code{merge_directed_flows}
 #' prior to plotting, otherwise lines for different directions will be overlaid.
 #' @export
-dodgr_flowmap <- function (net, filename, linescale = 5)
+dodgr_flowmap <- function (net, filename, bbox = NULL, linescale = 5)
 {
     fmax <- max (net$flow)
-    bb <- c (min (net$from_lon), min (net$from_lat),
-             max (net$from_lon), max (net$from_lat)) %>%
-            osmplotr::get_bbox ()
+    if (is.null (bbox))
+        bbox <- c (min (net$from_lon), min (net$from_lat),
+                 max (net$from_lon), max (net$from_lat)) %>%
+                osmplotr::get_bbox ()
 
     cols <- colorRampPalette (c ("lawngreen", "red")) (30)
-    map <- osmplotr::osm_basemap (bb, bg = "gray10") %>%
+    map <- osmplotr::osm_basemap (bbox, bg = "gray10") %>%
         osmplotr::add_colourbar (colour = "gray95", zlims = c (0, fmax),
                                  col = cols) %>%
         osmplotr::add_axes (colour = "gray95")
