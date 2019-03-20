d65014b743c7191d3aa65a49b4f117a1a2dc7b27
mpadge <mark.padgham@email.com>
2019-03-20 13:03:53 +0100

    fix covr script on travis

--- scripts/covr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-Rscript -e "covr::codecov(line_exclusions = list('src/heaps/bheap.cpp',"\
-    "'src/heaps/bheap.h','src/heaps/fheap.h', 'src/heaps/fheap.cpp',"\
-    "'src/heaps/heap23.h', 'src/heaps/heap23.cpp', 'src/heaps/heap23_2.h',"\
-    "'src/heaps/heap.h', 'src/heaps/heap_lib.h', 'src/heaps/radixheap.h',"\
-    "'src/heaps/radixheap.cpp', 'src/heaps/triheap.h', 'src/heaps/triheap.cpp',"\
-    "'src/heaps/triheap_ext.h', 'src/heaps/triheap_ext.cpp', 'src/dgraph.cpp'),"\
-    "function_exclusions = c('dodgr_streetnet', 'graph_from_pts'))"
