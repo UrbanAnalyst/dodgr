333700042caf02fe4a1e6923ab4c2338012dffa7
mpadge <mark.padgham@email.com>
2019-04-06 22:20:59 +0200

    :rocket:graph-conversion

--- tests/testthat/test-graph-conversion.R
+++ tests/testthat/test-graph-conversion.R
@@ -12,3 +12,13 @@ test_that("tidyraph", {
     expect_equal (nrow (graph), igraph::ecount (graph_t))
 })
 
+test_that("sf", {
+    graph <- weight_streetnet (hampi)
+    expect_silent (gsfc <- dodgr_to_sfc (graph))
+    expect_is (gsfc, "list")
+    expect_equal (length (gsfc), 2)
+
+    gsf1 <- dodgr_to_sf (graph)
+    gsf2 <- sf::st_sf (gsfc$dat, geometry = gsfc$geometry, crs = 4326)
+    expect_identical (gsf1, gsf2)
+})
