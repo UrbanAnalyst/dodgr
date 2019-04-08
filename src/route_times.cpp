3e0ac7a5935681ce9c321be36c8c5f2306e709e1
mpadge <mark.padgham@email.com>
2019-04-08 16:03:37 +0200

    add isLess fn to order around circle

--- src/route_times.cpp
+++ src/route_times.cpp
@@ -2,6 +2,27 @@
 #include "route_times.h"
 
 
+// https://stackoverflow.com/questions/6989100/sort-points-in-clockwise-order
+
+bool routetimes::isLess (double x0, double y0, double xa, double ya,
+        double xb, double yb)
+{
+    if (xa >= 0.0 && xb < 0.0)
+        return true;
+    else if (xa == 0.0 && xb == 0.0)
+        return ya > yb;
+
+    double det = (xa - x0) * (yb - y0) - (xb - x0) * (ya - y0);
+    if (det < 0)
+        return true;
+    if (det > 0)
+        return false;
+
+    double d1 = (xa - x0) * (xa - x0) - (ya - x0) * (ya - y0);
+    double d2 = (xb - x0) * (xb - x0) - (yb - x0) * (yb - y0);
+    return d1 > d2;
+}
+
 //' rcpp_route_times
 //'
 //' @noRd
--- src/route_times.h
+++ src/route_times.h
@@ -6,4 +6,9 @@
 // [[Rcpp::depends(RcppParallel)]]
 #include <RcppParallel.h>
 
+namespace routetimes {
+bool isLess (double x0, double y0, double xa, double ya,
+        double xb, double yb);
+} // end namespace
+
 Rcpp::NumericMatrix rcpp_route_times (const Rcpp::DataFrame graph);
