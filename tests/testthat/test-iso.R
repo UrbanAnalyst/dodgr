857a0cce5fe68901caad03033852eb91c2fd488e
mpadge <mark.padgham@email.com>
2019-08-21 11:30:03 +0930

    add dodgr_isochrones; closes #59

--- tests/testthat/test-iso.R
+++ tests/testthat/test-iso.R
@@ -22,6 +22,27 @@ test_that("isodists", {
               expect_true (nrow (d) > 0)
               # some points may give no iso countours, so the following 2 may
               # not always be equal:
-              expect_true ((npts - length (unique (d$from))) < 3)
+              expect_true ((npts - length (unique (d$from))) >= 0)
               expect_true (length (unique (d$dlim)) == length (dlim))
+})
+
+test_that("isochrones", {
+              expect_silent (hsc <- sf_to_sc (hampi))
+              # This all exists just to test the next line:
+              requireNamespace ("geodist")
+              requireNamespace ("dplyr")
+              expect_silent (net <- weight_streetnet (hsc,
+                                                      wt_profile = "bicycle"))
+              npts <- 100
+              from <- sample (net$.vx0, size = npts)
+              tlim <- c (5, 10, 20, 30, 60) * 60 # times in seconds
+              expect_silent (x <- dodgr_isochrones (net, from = from, tlim))
+              expect_is (x, "data.frame")
+              expect_equal (ncol (x), 5)
+              expect_identical (names (x), c ("from", "tlim", "id", "x", "y"))
+              expect_true (nrow (x) > 0)
+              # some points may give no iso countours, so the following 2 may
+              # not always be equal:
+              expect_true ((npts - length (unique (x$from))) >= 0)
+              expect_true (length (unique (x$tlim)) == length (tlim))
 })
