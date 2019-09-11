85949121db189a0c93cf21e208ae9149ce4c454f
mpadge <mark.padgham@email.com>
2019-09-11 11:43:30 +0200

    :balloon:iso tests

--- tests/testthat/test-iso.R
+++ tests/testthat/test-iso.R
@@ -26,6 +26,25 @@ test_that("isodists", {
               expect_true (length (unique (d$dlim)) <= length (dlim))
 })
 
+test_that ("turn penalty", {
+              hsc <- sf_to_sc (hampi)
+              net0 <- weight_streetnet (hsc, wt_profile = "bicycle")
+              npts <- 100
+              from <- sample (net0$.vx0, size = npts)
+              dlim <- c (1, 2, 5, 10, 20) * 100
+              d0 <- dodgr_isodists (net0, from = from, dlim)
+
+              net <- weight_streetnet (hsc, wt_profile = "bicycle",
+                                       turn_penalty = TRUE)
+              expect_true (nrow (net) > nrow (net0))
+              d <- dodgr_isodists (net, from = from, dlim)
+              # d includes compound vertices with "_start" suffix, and routes
+              # differently because of turning angles
+              expect_false (identical (d0, d))
+              expect_true (length (grep ("_start", d$from)) > 0)
+              expect_false (length (grep ("_start", d0$from)) > 0)
+})
+
 test_that ("errors", {
               expect_silent (hsc <- sf_to_sc (hampi))
               expect_silent (net <- weight_streetnet (hsc,
@@ -37,6 +56,15 @@ test_that ("errors", {
                             "dlim must be specified")
               expect_error (d <- dodgr_isodists (net, from = from, dlim = "a"),
                             "dlim must be numeric")
+              expect_error (d <- dodgr_isoverts (net, from = from,
+                                                 dlim = dlim, tlim = 500),
+                            "Only one of dlim or tlim can be provided")
+
+              net <- weight_streetnet (hampi)
+              expect_error (d <- dodgr_isochrones (net, from = from, tlim = 500),
+                            "isochrones can only be calculated from SC-class networks")
+              expect_error (d <- dodgr_isoverts (net, from = from, tlim = 500),
+                            "isoverts can only be calculated from SC-class networks")
              })
 
 test_that("isochrones", {
@@ -73,4 +101,10 @@ test_that("isoverts", {
               # dd has all vertices within isodistance hulls; d has only those
               # on the actual hulls, so far fewer vertices
               expect_true (nrow (d) > nrow (dd))
+
+              tlim <- c (60, 120, 300)
+              expect_silent (v <- dodgr_isoverts (net, from = from, tlim = tlim))
+              expect_true ("tlim" %in% names (v))
+              expect_true ("dlim" %in% names (d))
+              expect_true (all (paste0 (tlim) %in% unique (v$tlim)))
 })
