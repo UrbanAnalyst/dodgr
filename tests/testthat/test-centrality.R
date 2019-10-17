39ebb251d50ea59f3ff2028853a42c5ddae975e4
mpadge <mark.padgham@email.com>
2019-10-17 09:51:08 +0200

    test centrality

---
+++ tests/testthat/test-centrality.R
@@ -0,0 +1,24 @@
+context("centrality")
+
+test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
+             identical (Sys.getenv ("TRAVIS"), "true"))
+
+testthat::skip_on_cran ()
+
+test_that("centrality", {
+    graph_full <- weight_streetnet (hampi)
+    graph <- dodgr_contract_graph (graph_full)
+
+    graphc <- dodgr_centrality (graph)
+    expect_equal (nrow (graph), nrow (graphc))
+    expect_equal (ncol (graph) + 1, ncol (graphc))
+    expect_true ("centrality" %in% names (graphc))
+    expect_false ("centrality" %in% names (graph))
+
+    v <- dodgr_vertices (graph)
+    vc <- dodgr_centrality (graph, edges = FALSE)
+    expect_equal (nrow (v), nrow (vc))
+    expect_equal (ncol (v) + 1, ncol (vc))
+    expect_true ("centrality" %in% names (vc))
+    expect_false ("centrality" %in% names (v))
+})
