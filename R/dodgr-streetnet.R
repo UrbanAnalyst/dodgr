0875580b846dac69ab975e83d2ff7793e360aa06
mpadge <mark.padgham@email.com>
2021-07-18 21:46:15 +0200

    adjust expanded bbox limits beyond 90/180 degrees for #156

--- R/dodgr-streetnet.R
+++ R/dodgr-streetnet.R
@@ -162,6 +162,15 @@ process_bbox <- function (bbox,
     } else
         stop ("Either bbox or pts must be specified.")
 
+    if (bbox [1, 1] < -90)
+        bbox [1, 1] <- -90
+    if (bbox [2, 1] > 90)
+        bbox [2, 1] <- 90
+    if (bbox [1, 2] < 180)
+        bbox [1, 2] <- bbox [1, 2] + 360
+    if (bbox [2, 2] > 180)
+        bbox [2, 2] <- bbox [2, 2] - 360
+
     list (bbox = bbox, bbox_poly = bbox_poly)
 }
 
