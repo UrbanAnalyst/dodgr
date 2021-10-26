87dc841f2d2f524a9a9c239c1efa796599125f8b
mpadge <mark.padgham@email.com>
2021-10-26 10:52:31 +0200

    test load_streetnet for #170

--- tests/testthat/test-save-load.R
+++ tests/testthat/test-save-load.R
@@ -8,11 +8,11 @@ test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
 if (!test_all)
     RcppParallel::setThreadOptions (numThreads = 2)
 
-test_that("save", {
-    net <- weight_streetnet (hampi)
+test_that("save and load", {
+    net0 <- weight_streetnet (hampi)
     f <- file.path (tempdir (), "junk")
     expect_silent (
-        dodgr_save_streetnet (net, f)
+        dodgr_save_streetnet (net0, f)
         )
     expect_false (file.exists (f))
     f <- paste0 (f, ".Rds")
@@ -24,4 +24,13 @@ test_that("save", {
     expect_identical (names (x),
                       c ("graph", "verts", "graph_c",
                          "verts_c", "edge_map", "junctions"))
+
+    flist0 <- list.files (tempdir (), pattern = "^dodgr\\_")
+    clear_dodgr_cache ()
+
+    net1 <- dodgr_load_streetnet (f)
+    expect_equal (net0, net1)
+    flist1 <- list.files (tempdir (), pattern = "^dodgr\\_")
+
+    expect_true (all (flist1 %in% flist0))
 })
