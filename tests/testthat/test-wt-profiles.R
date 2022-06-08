e15aebc30fa848ba551c6d1206c0f2dd60d9bfe8
mpadge <mark.padgham@email.com>
2022-06-08 12:09:10 +0200

    more fixes to test-wt-profiles for cran machines

--- tests/testthat/test-wt-profiles.R
+++ tests/testthat/test-wt-profiles.R
@@ -3,7 +3,9 @@ context("weighting profiles")
 test_that("wp", {
               f <- file.path (tempdir (), "wp")
               expect_false (file.exists (paste0 (f, ".json")))
-              expect_silent (write_dodgr_wt_profile (f))
+              #expect_silent ( # pkg startup msgs on some systems
+                             write_dodgr_wt_profile (f)
+              #)
               expect_true (file.exists (paste0 (f, ".json")))
               w <- read_dodgr_wt_profile (f)
               expect_identical (w, dodgr::weighting_profiles)
