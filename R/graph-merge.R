356ed67d3c4813fd14251ac852027101ea28b628
mpadge <mark.padgham@email.com>
2022-06-10 11:42:41 +0200

    spaceout R/graph-merge.R

--- R/graph-merge.R
+++ R/graph-merge.R
@@ -23,26 +23,32 @@
 #' to <- sample (graph$to_id, size = 5)
 #' to <- to [!to %in% from]
 #' flows <- matrix (10 * runif (length (from) * length (to)),
-#'                  nrow = length (from))
+#'     nrow = length (from)
+#' )
 #' graph <- dodgr_flows_aggregate (graph, from = from, to = to, flows = flows)
 #' # graph then has an additonal 'flows` column of aggregate flows along all
 #' # edges. These flows are directed, and can be aggregated to equivalent
 #' # undirected flows on an equivalent undirected graph with:
 #' graph_undir <- merge_directed_graph (graph)
 #' # This graph will only include those edges having non-zero flows, and so:
-#' nrow (graph); nrow (graph_undir) # the latter is much smaller
+#' nrow (graph)
+#' nrow (graph_undir) # the latter is much smaller
 merge_directed_graph <- function (graph, col_names = c ("flow")) {
 
     # auto-detect either flow or centrality as col_names:
     if (length (col_names) == 1) {
         if (col_names == "flow" & !"flow" %in% names (graph) &
-            "centrality" %in% names (graph))
+            "centrality" %in% names (graph)) {
             col_names <- "centrality"
+        }
     }
-    if (!all (col_names %in% names (graph)))
-        stop (paste0 ("col_names [",
-                      paste (col_names, collapse = ", "),
-                      "] do not match columns in graph"))
+    if (!all (col_names %in% names (graph))) {
+        stop (paste0 (
+            "col_names [",
+            paste (col_names, collapse = ", "),
+            "] do not match columns in graph"
+        ))
+    }
 
     gr_cols <- dodgr_graph_cols (graph)
     graph2 <- convert_graph (graph, gr_cols) # nolint
@@ -52,9 +58,10 @@ merge_directed_graph <- function (graph, col_names = c ("flow")) {
     })
     res <- do.call (cbind, res)
     index <- which (rowSums (res) > 0)
-    graph <- graph [index, , drop = FALSE] #nolint
-    for (i in seq (col_names))
-        graph [[col_names [i] ]] <- res [index, i] # nolint
+    graph <- graph [index, , drop = FALSE] # nolint
+    for (i in seq (col_names)) {
+        graph [[col_names [i]]] <- res [index, i]
+    } # nolint
     class (graph) <- c (class (graph), "dodgr_merged")
 
     attr (graph, "hash") <- digest::digest (graph [[gr_cols$edge_id]])
