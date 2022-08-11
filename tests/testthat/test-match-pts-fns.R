60a9dcf1846e345e6661ac97849d9f4ceaf4c792
mpadge <mark.padgham@email.com>
2022-08-11 16:42:22 +0200

    split some test-graph-fns off to new test-match-pts-fns.R

---
+++ tests/testthat/test-match-pts-fns.R
@@ -0,0 +1,66 @@
+test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
+    identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
+
+skip_if (!test_all)
+
+dodgr_cache_off ()
+clear_dodgr_cache ()
+
+test_that ("points to graph", {
+
+    bb <- attr (hampi$geometry, "bbox")
+    n <- 100
+    x <- bb [1] + (bb [3] - bb [1]) * runif (n)
+    y <- bb [2] + (bb [4] - bb [2]) * runif (n)
+    pts <- data.frame (x = x, y = y)
+    net <- weight_streetnet (hampi)
+    expect_message (
+        index1 <- match_pts_to_verts (net, pts),
+        paste0 (
+            "First argument to match_pts_to_verts should ",
+            "be result of dodgr_vertices"
+        )
+    )
+
+    v <- dodgr_vertices (net)
+    expect_silent (index2 <- match_pts_to_verts (v, pts))
+    expect_identical (index1, index2)
+
+    colnames (pts) <- NULL
+    expect_message (
+        index3 <- match_pts_to_verts (v, pts),
+        "xy has no named columns; assuming order is x then y"
+    )
+    expect_identical (index1, index3)
+
+    pts <- data.frame (x = x, y = y, x2 = x)
+    expect_error (
+        index4 <- match_pts_to_verts (v, list (pts)),
+        "xy must be a matrix or data.frame"
+    )
+    expect_error (
+        index4 <- match_pts_to_verts (v, pts),
+        "xy must have only two columns"
+    )
+
+    pts <- data.frame (x = x, y = y)
+    expect_silent (index4 <- match_pts_to_verts (v, pts, connected = TRUE))
+    expect_true (!identical (index1, index4))
+
+    class (pts) <- c (class (pts), "tbl")
+    expect_silent (index5 <- match_pts_to_verts (v, pts, connected = TRUE))
+    expect_identical (index4, index5)
+
+    pts <- sf::st_as_sf (pts, coords = c (1, 2), crs = 4326)
+    expect_silent (index6 <- match_pts_to_verts (v, pts, connected = TRUE))
+    expect_identical (index4, index6)
+    expect_silent (index7 <- match_pts_to_verts (v, pts, connected = TRUE))
+    expect_identical (index4, index7)
+
+    pts <- hampi [1, ]
+    expect_error (index7 <- match_pts_to_verts (v, pts))
+    # error is "xy$geometry must be a collection of sfc_POINT objects", but
+    # expect_error does not match on the "$" symbo, but expect_error does not
+    # match on the "$" symbol
+
+})
