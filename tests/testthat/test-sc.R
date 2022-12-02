ed865d52915f8e2dd1892e0a8b503080123ebc30
mpadge <mark.padgham@email.com>
2022-12-02 12:10:59 +0100

    lint test-sc.R

--- tests/testthat/test-sc.R
+++ tests/testthat/test-sc.R
@@ -47,13 +47,13 @@ test_that ("SC", {
     expect_silent (v0 <- dodgr_vertices (net_sc2))
     # force re-cache by re-generating edge IDs:
     net_sc2$edge_ <-
-        paste0 (seq (nrow (net_sc2)) [order (runif (nrow (net_sc2)))])
+        paste0 (seq_len (nrow (net_sc2)) [order (runif (nrow (net_sc2)))])
     net_sc2$.vx0 <- as.factor (net_sc2$.vx0)
     expect_silent (v1 <- dodgr_vertices (net_sc2)) # should still work
 
     # force re-cache by re-generating edge IDs:
     net_sc2$edge_ <-
-        paste0 (seq (nrow (net_sc2)) [order (runif (nrow (net_sc2)))])
+        paste0 (seq_len (nrow (net_sc2)) [order (runif (nrow (net_sc2)))])
     net_sc2$.vx0 <- as.character (net_sc2$.vx0)
     net_sc2$.vx1 <- as.factor (net_sc2$.vx1)
     expect_silent (v2 <- dodgr_vertices (net_sc2)) # should still work
@@ -62,11 +62,11 @@ test_that ("SC", {
     net_sc3 <- dodgr_components (net_sc3)
     # force re-cache by re-generating edge IDs:
     net_sc3$edge_ <-
-        paste0 (seq (nrow (net_sc3)) [order (runif (nrow (net_sc3)))])
+        paste0 (seq_len (nrow (net_sc3)) [order (runif (nrow (net_sc3)))])
     expect_silent (v0 <- dodgr_vertices (net_sc3))
     expect_true (all (c ("x", "y") %in% names (v0)))
     net_sc3$edge_ <-
-        paste0 (seq (nrow (net_sc3)) [order (runif (nrow (net_sc3)))])
+        paste0 (seq_len (nrow (net_sc3)) [order (runif (nrow (net_sc3)))])
     net_sc3$.vx0_x <-
         net_sc3$.vx0_y <-
         net_sc3$.vx1_x <-
@@ -168,7 +168,10 @@ test_that ("contract with turn angles", {
             flows = fmat,
             contract = FALSE
         ),
-        "graphs with turn penalties should be submitted in full, not contracted form"
+        paste0 (
+            "graphs with turn penalties should be ",
+            "submitted in full, not contracted form"
+        )
     )
     expect_silent (
         graphtf <- dodgr_flows_aggregate (
@@ -197,7 +200,10 @@ test_that ("contract with turn angles", {
                 from = pts,
                 dens = rep (1, n)
             ),
-        "graphs with turn penalties should be submitted in full, not contracted form"
+        paste0 (
+            "graphs with turn penalties should be ",
+            "submitted in full, not contracted form"
+        )
     )
     expect_silent (
         graphtf <- dodgr_flows_disperse (grapht, from = pts, dens = rep (1, n))
@@ -248,7 +254,10 @@ test_that ("dodgr_times", {
     t1 <- dodgr_times (net_sc2, from = from, to = to)
     expect_warning (
         t2 <- dodgr_times (net_sc2_c, from = from, to = to),
-        "graphs with turn penalties should be submitted in full, not contracted form"
+        paste0 (
+            "graphs with turn penalties should be ",
+            "submitted in full, not contracted form"
+        )
     )
 
     dtime <- max (abs (t1 - t2), na.rm = TRUE)
