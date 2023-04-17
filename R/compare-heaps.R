1ed8b7c2e628dfb1d35fdf1641816815569cce89
mpadge <mark.padgham@email.com>
2023-04-17 12:37:18 +0200

    update compare-heaps.R docs for #202

--- R/compare-heaps.R
+++ R/compare-heaps.R
@@ -1,7 +1,7 @@
-#' compare_heaps
+#' Compare timings of different sort heaps for a given input graph.
 #'
 #' Perform timing comparison between different kinds of heaps as well as with
-#' equivalent `igraph` routine `distances`. To do this, a random
+#' equivalent routines from the \pkg{igraph} package. To do this, a random
 #' sub-graph containing a defined number of vertices is first selected.
 #' Alternatively, this random sub-graph can be pre-generated with the
 #' `dodgr_sample` function and passed directly.
