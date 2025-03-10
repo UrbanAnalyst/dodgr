3be3e72b27ca9c8f150bad64e22aa3cee2d300be
mpadge <mark.padgham@email.com>
2025-03-10 09:39:34 +0100

    condense lines from #278

--- R/graph-functions.R
+++ R/graph-functions.R
@@ -503,11 +503,9 @@ insert_one_edge <- function (graph, index, x, y, gr_cols) {
     graph [index + 1, gr_cols$d_weighted] <- d2 * wt
 
     if (!is.na (gr_cols$time)) {
-        graph [index, gr_cols$time] <- d1 *
-            time_scale
+        graph [index, gr_cols$time] <- d1 * time_scale
         graph [index + 1, gr_cols$time] <- d2 * time_scale
-        graph [index, gr_cols$time_weighted] <-
-            d1 * time_scale * time_wt
+        graph [index, gr_cols$time_weighted] <- d1 * time_scale * time_wt
         graph [index + 1, gr_cols$time_weighted] <- d2 * time_scale * time_wt
     }
 
