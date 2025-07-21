9eba49b5e6fae915d65245d8c4162deedfd4771a
mpadge <mark.padgham@email.com>
2025-07-21 15:11:29 +0200

    improve interpretation of 'oneway' flags for #299
    And add warning where flags might be ambiguous

--- R/weight-streetnet-times.R
+++ R/weight-streetnet-times.R
@@ -117,11 +117,16 @@ convert_hw_types_to_bool <- function (graph, wt_profile) {
 
 set_oneway_bike_flags <- function (graph, bikeflags, wt_profile) {
 
-    index <- which (!graph$oneway %in% c ("no", "yes"))
-    if (length (index) > 0) {
-        graph$oneway [index] <- "no"
+    oneway_keys <- tolower (substring (names (table (graph$oneway)), 1, 1))
+    if (length (oneway_keys) > 2L) {
+        warning ("'oneway' column has ambiguous values; should be TRUE/FALSE or 'yes'/'no' only")
     }
-    graph$oneway <- ifelse (graph$oneway == "no", FALSE, TRUE)
+    if (all (oneway_keys %in% c ("0", "1"))) {
+        graph$oneway <- as.logical (graph$oneway)
+    } else if (all (oneway_keys %in% c ("y", "n"))) {
+        graph$oneway <- tolower (substring (graph$oneway, 1, 1))
+        graph$oneway <- ifelse (graph$oneway == "no", FALSE, TRUE)
+    }
 
     if (length (bikeflags) == 1) {
         # oneway:bicycle doesn't enquote properly, so:
